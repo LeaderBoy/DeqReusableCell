1e10a44e59e6dd78e9bfefed04e3a7e24b293853
LeaderBoy <748603349@qq.com>
2019-09-02 14:07:48 +0800

    1.initial

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # DeqReusableCell
 
-A description of this package.
+### 注册复用的cell
+
+正常情况下使用tableview查找复用cell
+```swift
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  var cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+  if cell == nil {
+    cell = UITableView(frame: self.bounds, style: UITableView.Style.grouped)
+  }
+  return cell!
+}
+```
+
+### 使用本文件注册
+```swift
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  let cell = tableView.dequeueReusableClassCell(UITableViewCell.self)
+  return cell
+}
+```
+
+
--- Sources/DeqReusableCell/DeqReusableCell.swift
+++ Sources/DeqReusableCell/DeqReusableCell.swift
@@ -1,3 +1,83 @@
-struct DeqReusableCell {
-    var text = "Hello, World!"
+import UIKit
+
+
+// MARK: - 查找复用cell(代码创建和xib创建)的便利方法
+extension UITableViewCell {
+    static var identifier : String {
+        return String(describing: self)
+    }
+    
+    static var nib : UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
 }
+
+extension UITableView {
+    
+    /// 查找使用代码编写的复用cell
+    ///
+    /// - Parameters:
+    ///   - cellType: 传递 YourCell.self
+    ///   - style: UITableViewCellStyle 默认为default
+    /// - Returns: YourCell的实例
+    func dequeueReusableClassCell<T : UITableViewCell>(_ cellType : T.Type, style : UITableViewCell.CellStyle = .default) -> T {
+        let identifier = cellType.identifier
+        var cell = dequeueReusableCell(withIdentifier: identifier)
+        if cell == nil {
+            cell = T(style: style, reuseIdentifier: identifier)
+        }
+        return cell as! T
+    }
+    
+    /// 查找xib创建的复用的cell
+    ///
+    /// - Parameters:
+    ///   - cellType: 传递 YourCell.self
+    ///   - owner: loadNibNamed的参数owner,一般传递self
+    /// - Returns: YourCell的实例
+    func dequeueReusableNibCell<T : UITableViewCell>(_ cellType : T.Type,owner: Any?) -> T {
+        let identifier = cellType.identifier
+        var cell = dequeueReusableCell(withIdentifier: identifier)
+        if cell == nil {
+            let wraperCell = Bundle.main.loadNibNamed(identifier, owner: owner, options: nil)?.first
+            guard let unwrap = wraperCell as? T else {
+                fatalError("cell is nil or cell can not be converted to \(type(of: T.self))")
+            }
+            cell = unwrap
+        }
+        return cell as! T
+    }
+}
+
+
+extension UICollectionViewCell {
+    static var identifier : String {
+        return String(describing: self)
+    }
+    
+    static var nib : UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}
+
+extension UICollectionView {
+    
+    /// 注册UICollectionViewCell
+    /// - Parameter cellType: 传递 YourCell.self
+    func registerClass<T : UICollectionViewCell>(cellType: T.Type) {
+        register(cellType.classForCoder(), forCellWithReuseIdentifier: cellType.identifier)
+    }
+    
+    func registerNib<T : UICollectionViewCell>(cellType: T.Type) {
+        register(cellType.nib, forCellWithReuseIdentifier: cellType.identifier)
+    }
+    
+    func dequeueReusableCell<T : UICollectionViewCell>(_ cellType : T.Type,indexPath: IndexPath) -> T {
+        let cell = dequeueReusableCell(withReuseIdentifier: cellType.identifier, for: indexPath)
+        guard let unwrap = cell as? T else {
+            fatalError("cell is nil or cell can not be converted to \(type(of: T.self))")
+        }
+        return unwrap
+    }
+}
+
